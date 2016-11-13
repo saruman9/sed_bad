3920a20eb1a2cd7673f2daa05e4fa27ce6dfb882
saruman9 <rum.274.4@gmail.com>
2016-11-13 15:19:52 +0300

    Added new UI elements.
    Added toolbar, list of directories, calendar, list of tickets.

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -23,6 +23,12 @@ pub struct MainUI {
 
     menu_bar: gtk::MenuBar,
 
+    toolbar: gtk::Toolbar,
+    create_ticket_t_button: gtk::ToolButton,
+    create_directory_t_button: gtk::ToolButton,
+    open_ticket_t_button: gtk::ToolButton,
+    edit_access_t_button: gtk::ToolButton,
+
     file_menu_item: gtk::MenuItem,
     file_menu: gtk::Menu,
     log_in_menu_item: gtk::MenuItem,
@@ -36,6 +42,31 @@ pub struct MainUI {
 
     window: gtk::Window,
     v_box: gtk::Box,
+    h_box: gtk::Box,
+
+    dir_cal_box: gtk::Box,
+    directories_list: gtk::ListBox,
+    calendar: gtk::Calendar,
+
+    tickets_scrolled_window: gtk::ScrolledWindow,
+    tickets_tree_view: gtk::TreeView,
+    tickets_list_store: gtk::ListStore,
+
+    id_column: gtk::TreeViewColumn,
+    completed_column: gtk::TreeViewColumn,
+    name_column: gtk::TreeViewColumn,
+    author_column: gtk::TreeViewColumn,
+    ctime_column: gtk::TreeViewColumn,
+    mtime_column: gtk::TreeViewColumn,
+    responsible_column: gtk::TreeViewColumn,
+
+    id_cell: gtk::CellRendererText,
+    completed_cell: gtk::CellRendererToggle,
+    name_cell: gtk::CellRendererText,
+    author_cell: gtk::CellRendererText,
+    ctime_cell: gtk::CellRendererText,
+    mtime_cell: gtk::CellRendererText,
+    responsible_cell: gtk::CellRendererText,
 }
 
 impl MainUI {
@@ -49,6 +80,32 @@ impl MainUI {
                 .expect("Error of creating database."))),
 
             menu_bar: gtk::MenuBar::new(),
+
+            toolbar: gtk::Toolbar::new(),
+            create_ticket_t_button: gtk::ToolButton::new(
+                Some(
+                    &gtk::Image::new_from_icon_name("document-new", gtk::IconSize::LargeToolbar
+                                                    .into())
+                ),
+                Some("Create ticket")),
+            create_directory_t_button: gtk::ToolButton::new(
+                Some(
+                    &gtk::Image::new_from_icon_name("folder-new", gtk::IconSize::LargeToolbar
+                                                    .into())
+                ),
+                Some("Create directory")),
+            open_ticket_t_button: gtk::ToolButton::new(
+                Some(
+                    &gtk::Image::new_from_icon_name("document-open", gtk::IconSize::LargeToolbar
+                                                    .into())
+                ),
+                Some("Open ticket")),
+            edit_access_t_button: gtk::ToolButton::new(
+                Some(
+                    &gtk::Image::new_from_icon_name("dialog-password", gtk::IconSize::LargeToolbar
+                                                    .into())
+                ),
+                Some("Edit access")),
 
             file_menu_item: gtk::MenuItem::new_with_mnemonic("_File"),
             file_menu: gtk::Menu::new(),
@@ -64,6 +121,37 @@ impl MainUI {
 
             window: gtk::Window::new(gtk::WindowType::Toplevel),
             v_box: gtk::Box::new(gtk::Orientation::Vertical, 0),
+            h_box: gtk::Box::new(gtk::Orientation::Horizontal, 0),
+
+            dir_cal_box: gtk::Box::new(gtk::Orientation::Vertical, 0),
+            directories_list: gtk::ListBox::new(),
+            calendar: gtk::Calendar::new(),
+
+            tickets_scrolled_window: gtk::ScrolledWindow::new(None, None),
+            tickets_tree_view: gtk::TreeView::new(),
+            tickets_list_store: gtk::ListStore::new(&[gtk::Type::String, // Id.
+                                                      gtk::Type::Bool, // Completed.
+                                                      gtk::Type::String, // Name.
+                                                      gtk::Type::String, // Author.
+                                                      gtk::Type::String, // Create time.
+                                                      gtk::Type::String, // Modification time.
+                                                      gtk::Type::String]), // Responsible.
+
+            id_column: gtk::TreeViewColumn::new(),
+            completed_column: gtk::TreeViewColumn::new(),
+            name_column: gtk::TreeViewColumn::new(),
+            author_column: gtk::TreeViewColumn::new(),
+            ctime_column: gtk::TreeViewColumn::new(),
+            mtime_column: gtk::TreeViewColumn::new(),
+            responsible_column: gtk::TreeViewColumn::new(),
+
+            id_cell: gtk::CellRendererText::new(),
+            completed_cell: gtk::CellRendererToggle::new(),
+            name_cell: gtk::CellRendererText::new(),
+            author_cell: gtk::CellRendererText::new(),
+            ctime_cell: gtk::CellRendererText::new(),
+            mtime_cell: gtk::CellRendererText::new(),
+            responsible_cell: gtk::CellRendererText::new(),
         };
         tmp.setup();
         tmp.connect_signals();
@@ -78,7 +166,94 @@ impl MainUI {
     }
 
     fn setup(&self) {
+        self.setup_columns();
+        self.setup_tickets_list_store();
+        self.setup_directories_list();
         self.setup_window();
+    }
+
+    fn setup_columns(&self) {
+        // Set title.
+        self.id_column.set_title("Id");
+        self.completed_column.set_title("C");
+        self.name_column.set_title("Name");
+        self.author_column.set_title("Author");
+        self.ctime_column.set_title("Create");
+        self.mtime_column.set_title("Modification");
+        self.responsible_column.set_title("Responsible");
+
+        // Set resizable.
+        self.id_column.set_resizable(true);
+        self.completed_column.set_resizable(true);
+        self.name_column.set_resizable(true);
+        self.author_column.set_resizable(true);
+        self.ctime_column.set_resizable(true);
+        self.mtime_column.set_resizable(true);
+        self.responsible_column.set_resizable(true);
+
+        // Attach cell to column.
+        self.id_column.pack_start(&self.id_cell, true);
+        self.completed_column.pack_start(&self.completed_cell, true);
+        self.name_column.pack_start(&self.name_cell, true);
+        self.author_column.pack_start(&self.author_cell, true);
+        self.ctime_column.pack_start(&self.ctime_cell, true);
+        self.mtime_column.pack_start(&self.mtime_cell, true);
+        self.responsible_column.pack_start(&self.responsible_cell, true);
+
+        // Add attribute.
+        self.id_column.add_attribute(&self.id_cell, "text", 0);
+        self.completed_column.add_attribute(&self.completed_cell, "active", 1);
+        self.name_column.add_attribute(&self.name_cell, "text", 2);
+        self.author_column.add_attribute(&self.author_cell, "text", 3);
+        self.ctime_column.add_attribute(&self.ctime_cell, "text", 4);
+        self.mtime_column.add_attribute(&self.mtime_cell, "text", 5);
+        self.responsible_column.add_attribute(&self.responsible_cell, "text", 6);
+
+        // Set clickable.
+        self.id_column.set_clickable(true);
+        self.completed_column.set_clickable(true);
+        self.name_column.set_clickable(true);
+        self.author_column.set_clickable(true);
+        self.ctime_column.set_clickable(true);
+        self.mtime_column.set_clickable(true);
+        self.responsible_column.set_clickable(true);
+
+        // Set reorderable.
+        self.id_column.set_reorderable(true);
+        self.completed_column.set_reorderable(true);
+        self.name_column.set_reorderable(true);
+        self.author_column.set_reorderable(true);
+
+        self.ctime_column.set_reorderable(true);
+        self.mtime_column.set_reorderable(true);
+        self.responsible_column.set_reorderable(true);
+
+        // Set sort column.
+        self.id_column.set_sort_column_id(0);
+        self.completed_column.set_sort_column_id(1);
+        self.name_column.set_sort_column_id(2);
+        self.author_column.set_sort_column_id(3);
+        self.ctime_column.set_sort_column_id(4);
+        self.mtime_column.set_sort_column_id(5);
+        self.responsible_column.set_sort_column_id(6);
+
+        self.tickets_tree_view.append_column(&self.id_column);
+        self.tickets_tree_view.append_column(&self.completed_column);
+        self.tickets_tree_view.append_column(&self.name_column);
+        self.tickets_tree_view.append_column(&self.author_column);
+        self.tickets_tree_view.append_column(&self.ctime_column);
+        self.tickets_tree_view.append_column(&self.mtime_column);
+        self.tickets_tree_view.append_column(&self.responsible_column);
+    }
+
+    fn setup_tickets_list_store(&self) {
+        self.tickets_tree_view.set_model(Some(&self.tickets_list_store));
+    }
+
+    fn setup_directories_list(&self) {
+        self.directories_list.insert(&gtk::Label::new(Some("Inbox")), 0);
+        self.directories_list.insert(&gtk::Label::new(Some("Outbox")), 1);
+        self.directories_list.insert(&gtk::Label::new(Some("Favorites")), 2);
     }
 
     fn setup_window(&self) {
@@ -142,6 +317,7 @@ impl MainUI {
 
     fn pack_and_show(&self) {
         self.pack_menu();
+        self.pack_toolbar();
         self.pack_v_box();
         self.pack_window();
     }
@@ -176,10 +352,47 @@ impl MainUI {
         self.menu_bar.append(&self.root_menu_item);
     }
 
+    fn pack_toolbar(&self) {
+        use gtk::ContainerExt;
+
+        self.toolbar.add(&self.create_ticket_t_button);
+        self.toolbar.add(&self.create_directory_t_button);
+        self.toolbar.add(&gtk::SeparatorToolItem::new());
+        self.toolbar.add(&self.open_ticket_t_button);
+        self.toolbar.add(&self.edit_access_t_button);
+    }
+
     fn pack_v_box(&self) {
         use gtk::BoxExt;
 
         self.v_box.pack_start(&self.menu_bar, false, false, 0);
+        self.v_box.pack_start(&self.toolbar, false, false, 0);
+        self.v_box.pack_start(&self.h_box, true, true, 0);
+
+        self.pack_h_box();
+    }
+
+    fn pack_h_box(&self) {
+        use gtk::BoxExt;
+
+        self.h_box.pack_start(&self.dir_cal_box, false, false, 0);
+        self.h_box.pack_start(&self.tickets_scrolled_window, true, true, 0);
+
+        self.pack_dir_cal_box();
+        self.pack_tickets_scrolled_window();
+    }
+
+    fn pack_dir_cal_box(&self) {
+        use gtk::BoxExt;
+
+        self.dir_cal_box.pack_start(&self.directories_list, true, true, 0);
+        self.dir_cal_box.pack_start(&self.calendar, false, false, 0);
+    }
+
+    fn pack_tickets_scrolled_window(&self) {
+        use gtk::ContainerExt;
+
+        self.tickets_scrolled_window.add(&self.tickets_tree_view);
     }
 
     fn pack_window(&self) {
@@ -192,6 +405,7 @@ impl MainUI {
 
     pub fn update_ui(&self) {
         self.update_menu_bar();
+        self.update_toolbar();
         self.update_main();
     }
 
@@ -205,5 +419,23 @@ impl MainUI {
         }
     }
 
-    fn update_main(&self) {}
+    fn update_toolbar(&self) {
+        use gtk::WidgetExt;
+
+        if self.current_user.borrow().name() != "" {
+            self.toolbar.show_all();
+        } else {
+            self.toolbar.hide();
+        }
+    }
+
+    fn update_main(&self) {
+        use gtk::WidgetExt;
+
+        if self.current_user.borrow().name() != "" {
+            self.h_box.show_all();
+        } else {
+            self.h_box.hide();
+        }
+    }
 }
